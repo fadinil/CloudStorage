a7967e38ded48c903a89859fab4677e76ab7d302
Ilya F <91550079+fadinil@users.noreply.github.com>
2022-08-11 03:09:20 +0300

    Create server and connect database

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const PORT = 5000;
+const app = express()
+const DB_URL = 'mongodb+srv://user:user@cluster0.lnouucd.mongodb.net/?retryWrites=true&w=majority'
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200).json('Сервер работает')
+})
+
+async function startApp(){
+    try{
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log("Server started on port " + PORT))
+    } catch(e){
+        console.log(e)
+    }
+}
+
+startApp()
+
--- server/main.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-public class main {
-
-    public main() {
-        System.out.println("Привет мир!");
-    }
-
-}

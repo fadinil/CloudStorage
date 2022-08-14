a608e0823c7a5af22eb9b7cc83d88c7c217b0062
Ilya Sharapov <ilya.sharapov@stada.ru>
2022-08-14 18:09:46 +0300

    add UI components

---
+++ cloudstorage/src/components/UI/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="button">
+        <slot></slot>
+    </button>
+</template>
+
+
+
+<script>
+export default {
+  name: 'my-button'
+}
+</script>
+
+
+
+<style scoped>
+.button {
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 2px solid teal;
+}
+</style>
---
+++ cloudstorage/src/components/UI/index.js
@@ -0,0 +1,7 @@
+import MyInput from '@/components/UI/MyInput.vue';
+import MyButton from '@/components/UI/MyButton.vue';
+
+export default [
+    MyButton,
+    MyInput
+];

9a8a0a770b24bc735255aa5dd8df7b3f8abae63c
Ilya Sharapov <ilya.sharapov@stada.ru>
2022-08-14 18:49:17 +0300

    add navbar

--- cloudstorage/src/App.vue
+++ cloudstorage/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <my-navbar />
     <div class="app">
         <router-view />
     </div>
@@ -6,12 +7,15 @@
 
 
 <script>
-export default {
+import MyNavbar from './components/MyNavbar.vue';
 
+export default {
+    components: {
+        MyNavbar
+    }
 }
 </script>
 
 
 <style scoped>
-
 </style>
---
+++ cloudstorage/src/components/MyNavbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="navbar">
+        <div class="navbar__btns">
+            <my-button 
+            style="margin-left: 15px;"
+            >Войти</my-button>
+            <my-button 
+            style="margin-left: 30px;"
+            >Регистрация</my-button>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+}
+</script>
+
+
+
+<style scoped>
+.navbar {
+    height: 50;
+    background-color: rgb(228, 228, 228);
+    box-shadow: 2px 2px 4px gray;
+    display: flex;
+    align-items: center;
+    padding: 15px 15px;
+}
+
+.navbar__btns {
+    margin-left: auto;
+}
+</style>

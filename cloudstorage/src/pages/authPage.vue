333a5d91fb2d77bebb09caea52d3cc3cb80e239d
Ilya Sharapov <ilya.sharapov@stada.ru>
2022-08-14 19:22:51 +0300

    add fetchUsers

--- cloudstorage/src/pages/authPage.vue
+++ cloudstorage/src/pages/authPage.vue
@@ -1,20 +1,70 @@
 <template>
     <div>
-        <h1> Логин
-        Пароль</h1>
+        <signInForm />
+        <signUpForm />
+    </div>
+    <div v-for="(user, index) in users">
+        <div>
+            <p> {{ index }} </p>
+        </div>
+        <div>
+            <p>ID: {{ user.name }}</p>
+        </div>
+        <div>
+            <p>Имя: {{ user.username }}</p>
+        </div>
+        <div>
+            <p>Пароль: {{ user.email }} </p>
+        </div>
     </div>
 </template>
 
 
 
 <script>
-export default {
+import signInForm from '@/components/signInForm';
+import signUpForm from '@/components/signUpForm';
+import axios from 'axios';
 
+export default {
+    components: {
+        signInForm,
+        signUpForm
+    },
+    data() {
+        return {
+            users: [
+                // {
+                //     id: 1,
+                //     name: 'Илья',
+                //     password: 'sdvdf243'
+                // }
+            ]
+        }
+    },
+    methods: {
+        async fetchUsers() {
+            try {
+                const response = await axios.get(
+                    // "https://jsonplaceholder.typicode.com/users",
+                    "http://ip:5000/",
+                    
+                    // { headers: { "Access-Control-Allow-Origin": "*" } }
+                );
+                this.users = response.data;
+            } catch (error) {
+                alert("ОШИБКА");
+            } finally {
+            }
+        }
+    },
+    mounted() {
+        this.fetchUsers();
+    }
 }
 </script>
 
 
 
 <style scoped>
-
 </style>
